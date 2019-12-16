5d7ba97953d02fc1d87c15ac07d355ba45efd329
mostalo <mostloub@yahoo.com>
2019-12-16 23:44:45 +0100

    test add

--- src/app/addtst/addtst.page.ts
+++ src/app/addtst/addtst.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TstService } from '../services/tst.service';
 import { Validators, FormBuilder, FormGroup, FormControl} from '@angular/forms';
+import { Customer } from '../model/customer';
 import {NavParams, ModalController} from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-addtst',
@@ -9,23 +12,72 @@ import {NavParams, ModalController} from '@ionic/angular';
 })
 export class AddtstPage implements OnInit {
 
-  validations_form: FormGroup;
+  customerForm: FormGroup;
+  products: Customer;
+  productID: number;
+  editable: boolean = false;
 
-  constructor(private navpaams: NavParams, private modalcontroller: ModalController, public formbuilder: FormBuilder) { }
+  constructor(private navpaams: NavParams, private modalcontroller: ModalController, public formbuilder: FormBuilder, private tstservice: TstService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.validations_form = this.formbuilder.group({
-      name_customer: new FormControl(),
-      desc_customer: new FormControl()
+    this.customerForm = this.formbuilder.group({
+      //name_customer: new FormControl(),
+      //desc_customer: new FormControl()
+      name_customer: [
+        '',
+        [
+          Validators.required, // validação de campo requirido
+          Validators.minLength(4), // validação de minimo de caracteres
+          Validators.maxLength(100), // validação de maximo de caracteres
+        ]
+      ],
+      desc_customer: [
+        '',
+        [
+          Validators.required, // validação de campo requirido
+          Validators.minLength(4), // validação de minimo de caracteres
+          Validators.maxLength(100), // validação de maximo de caracteres
+        ]
+      ]
     });
+
+    this.route.paramMap.subscribe(params => {
+      this.productID =+ params.get('id');
+      if(this.productID) {
+        this.getProduto(this.productID);
+        this.editable = true;
+      }
+    })
+
+
   }
 
   closeModal(){
     this.modalcontroller.dismiss();
   }
-  onSubmit(values: any){
+  addProduct(){
+    let body = {
+      action: 'add',
+      name_customer : this.products.name_customer,
+      desc_customer : this.products.desc_customer
+    };
+    this.tstservice.postData(body, 'api.php').subscribe(data => {
+      //this.router.navigate(['/customers']);
+      //this.name_customer = ''
+      //this.desc_customer = ''
+      console.log('Ok');
+    });
+
+    //console.log(values);
+    //this.router.navigate(["/user"]);
+  }
+  editProduct(values: any){
     console.log(values);
     //this.router.navigate(["/user"]);
+  }
+
+  getProduto(id: number) {
+    console.log(id);
   }
 
 }
